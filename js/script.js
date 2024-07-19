2ab48720d9afee8e8ebe5ab1b74ab76e5032ed1c
Иван Исаченко <iisachenko@rusholts.local>
2024-07-19 19:50:19 +0300

    send

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
 
-if (document.documentElement.clientWidth > 768) {
+let buttonSh = document.querySelector('.main-button');
+
+function swiperOff() {
+    let listSliderDeact = document.querySelector('.main-list');
+
     if (swiper){
         swiper.destroy();
     }
+
+    listSliderDeact.classList.add('main-list--slider-deactive');
+    listSliderDeact.classList.remove('swiper-wrapper')
+    // listSliderDeact.style.maxWidth = (widthSize) + 'px';
+}
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    gap: 16,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+});
+
+if (document.documentElement.clientWidth > 768 && document.documentElement.clientWidth < 1120) {
     
-}else {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        spaceBetween: 16,
-        slidesPerView: 'auto',
-        gap: 16,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-    
-    });
+    swiperOff();
+
+} else if (document.documentElement.clientWidth > 1120){
+    swiperOff();
+} else {
+    swiper
 }
