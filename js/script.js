0989af52e65c178b49ad7240eaa3473f23c61760
Иван Исаченко <iisachenko@rusholts.local>
2024-07-22 18:03:09 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,39 +1,60 @@
-
-let buttonSh = document.querySelector('.main-button');
+let listSliderDeact = document.querySelector('.main-list');
+let buttonShow = document.querySelector('#showButtonWrapper');
+let buttonArrow = buttonShow.querySelector('#arrow');
+let element = buttonShow.querySelector('#showButton');
+let pagginationOff = document.querySelector('.swiper-pagination');
 
 function swiperOff() {
-    let listSliderDeact = document.querySelector('.main-list');
+  
 
-    if (swiper){
-        swiper.destroy();
-    }
+  if (swiper) {
+    swiper.destroy();
+  }
 
-    listSliderDeact.classList.add('main-list--slider-deactive');
-    listSliderDeact.classList.remove('swiper-wrapper')
-    // listSliderDeact.style.maxWidth = (widthSize) + 'px';
+  listSliderDeact.classList.add('main-list--slider-deactive');
+  listSliderDeact.classList.remove('swiper-wrapper');
+  pagginationOff.remove();
+}
+
+function showContent() {
+    buttonShow.addEventListener('click', function(evt) {
+        evt.preventDefault();
+
+        let contentShowing = document.querySelector('.main-list--show-content');
+       
+        if(!contentShowing){
+            listSliderDeact.classList.add('main-list--show-content');
+            element.textContent = 'Скрыть';
+            buttonArrow.classList.add('button-wrapper__arrow--reverse')
+        } else {
+            listSliderDeact.classList.remove('main-list--show-content');
+            element.textContent = 'Показать всё';
+            buttonArrow.classList.remove('button-wrapper__arrow--reverse')
+        }
+    });
 }
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 16,
-    slidesPerView: 'auto',
-    gap: 16,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 'auto',
 
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
 });
 
-if (document.documentElement.clientWidth > 768 && document.documentElement.clientWidth < 1120) {
-    
-    swiperOff();
-
-} else if (document.documentElement.clientWidth > 1120){
-    swiperOff();
+if (document.documentElement.clientWidth >= 768 ) {
+  swiperOff();
+  showContent();
+  
 } else {
-    swiper
+  swiper;
+  buttonShow.remove();
 }
